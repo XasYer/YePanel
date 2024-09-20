807b79325e554c7ccf80cb0174b50309b914b9fe
小叶 <1936472877@qq.com>
2024-09-20 16:12:45 +0800

    🎈 perf: Ciallo～(∠・ω< )⌒☆

--- src/App.vue
+++ src/App.vue
@@ -11,6 +11,21 @@ import { ElConfigProvider } from "element-plus";
 import { ReDialog } from "@/components/ReDialog";
 import zhCn from "element-plus/es/locale/lang/zh-cn";
 
+let OriginTitile = document.title;
+let titleTime;
+document.addEventListener("visibilitychange", () => {
+  if (document.hidden) {
+    OriginTitile = document.title;
+    document.title = "我才不是柚子厨呢";
+    clearTimeout(titleTime);
+  } else {
+    document.title = "Ciallo～(∠・ω< )⌒☆";
+    titleTime = setTimeout(() => {
+      document.title = OriginTitile;
+    }, 2000);
+  }
+});
+
 export default defineComponent({
   name: "app",
   components: {
