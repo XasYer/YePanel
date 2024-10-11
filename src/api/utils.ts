307e9bbeae4498b44a4658798a90973b00e8d708
小叶 <1936472877@qq.com>
2024-10-11 22:43:02 +0800

    fix: mock

--- src/api/utils.ts
+++ src/api/utils.ts
@@ -1,5 +1,4 @@
-let baseUrl =
-  localStorage.getItem("QQBotBaseUrl") || "http://127.0.0.1:2536/qqbot";
+let baseUrl = localStorage.getItem("QQBotBaseUrl") || "http://127.0.0.1:2877";
 
 export const setBaseUrlApi = (url: string) => {
   baseUrl = url;
